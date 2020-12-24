97c5457c424b252813acd53d32e32a6bd4471a06
Student Main <scncgz@gmail.com>
2020-12-24 10:11:56 +0800

    sip003 support

--- main.go
+++ main.go
@@ -3,24 +3,51 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
+	"os"
 	"strings"
 )
 
 var (
-	RunMode = flag.String("mode", "", "run mode. must be client or server")
-	LocalAddr = flag.String("local", "", "local address to listen")
+	RunMode    = flag.String("mode", "", "run mode. must be client or server")
+	LocalAddr  = flag.String("local", "", "local address to listen")
 	RemoteAddr = flag.String("remote", "", "remote address to connect")
-	CertPath = flag.String("cert", "", "(server) certificate (*.pem) path")
-	KeyPath = flag.String("key", "", "(server) certificate key (*.key) path")
+	CertPath   = flag.String("cert", "", "(server) certificate (*.pem) path")
+	KeyPath    = flag.String("key", "", "(server) certificate key (*.key) path")
 	ServerName = flag.String("sni", "", "(client) optionally override SNI")
 )
 
-func init() {
-	flag.Parse()
-}
-
 func main() {
+	options := map[string]string{}
+	_, sip003 := os.LookupEnv("SS_LOCAL_HOST")
+	if sip003 {
+		log.Println("start as SIP003 plugin, command line parameter is ignored.")
+		*LocalAddr = fmt.Sprintf("%s:%s", os.Getenv("SS_LOCAL_HOST"), os.Getenv("SS_LOCAL_PORT"))
+		*RemoteAddr = fmt.Sprintf("%s:%s", os.Getenv("SS_REMOTE_HOST"), os.Getenv("SS_REMOTE_PORT"))
+		optionArr := strings.Split(os.Getenv("SS_PLUGIN_OPTIONS"), ";")
+		if optionArr[0] != "server" {
+			*RunMode = "client"
+		} else {
+			*RunMode = "server"
+		}
+		optionArr = optionArr[1:]
+		for _, s := range optionArr {
+			kv := strings.Split(s, "=")
+			if len(kv) != 2 {
+				log.Println(s)
+				log.Fatalln("Can't parse plugin option")
+			}
+			options[kv[0]] = options[kv[1]]
+		}
+
+		*ServerName = readOption(options, "sni")
+		*CertPath = readOption(options, "cert")
+		*KeyPath = readOption(options, "key")
+	} else {
+		flag.Parse()
+	}
+
 	switch strings.ToLower(*RunMode) {
 	case "client":
 		GunServiceClientImpl{
@@ -38,4 +65,12 @@ func main() {
 	default:
 		log.Fatalf("invalid run mode. must be client or server.")
 	}
+}
+
+func readOption(options map[string]string, key string) string {
+	val, ok := options[key]
+	if !ok {
+		val = ""
+	}
+	return val
 }
